42f7b1ee2688860ae01c7a521c01d668492b5636
Julie Bureau <bureau.julie@ymail.com>
2019-02-16 13:47:54 +1300

    eslint

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const chalk = require("chalk");
-const clear = require("clear");
-const figlet = require("figlet");
-const files = require("./src/files");
-const inquirer = require("inquirer");
-const readline = require("readline");
+import chalk from 'chalk'
+import clear from 'clear'
+import figlet from 'figlet'
+import inquirer from 'inquirer'
+import readline from 'readline'
+import files from './src/files'
 
-clear();
+clear()
 
-console.log(chalk.green.dim(figlet.textSync("Duck wizard", { horizontalLayout: "full" })));
-files.checkInit().then(() => {
-    const rl = readline.createInterface({
+console.log( chalk.green.dim( figlet.textSync( 'Duck wizard', { horizontalLayout: 'full' } ) ) )
+files.checkInit().then( () => {
+    const rl = readline.createInterface( {
         input: process.stdin,
         output: process.stdout,
-        prompt: chalk.black.bgYellow(" $ DW $ ") + " > "
-    });
-    rl.prompt();
+        prompt: chalk.black.bgYellow( ' $ DW $ ' ) + ' > '
+    } )
+    rl.prompt()
 
-    rl.on("line", line => {
-        switch (line.trim()) {
-            case "hello":
-                console.log("world!");
-                break;
-            case "exit":
-                rl.close();
-                break;
+    rl.on( 'line', line => {
+        switch ( line.trim() ) {
+            case 'hello':
+                console.log( 'world!' )
+                break
+            case 'exit':
+                rl.close()
+                break
             default:
-                console.log(`Say what? I might have heard '${line.trim()}'`);
-                break;
+                console.log( `Say what? I might have heard '${line.trim()}'` )
+                break
         }
-        rl.prompt();
-    }).on("close", () => {
-        console.log("Have a great day!");
-        process.exit(0);
-    });
-});
+        rl.prompt()
+    } ).on( 'close', () => {
+        console.log( 'Have a great day!' )
+        process.exit( 0 )
+    } )
+} )
--- src/files.js
+++ src/files.js
@@ -1,40 +1,40 @@
-const fs = require("fs");
-const path = require("path");
-const inquirer = require("inquirer");
+const fs = require('fs')
+const path = require('path')
+const inquirer = require('inquirer')
 
 module.exports = {
     getCurrentDirectoryBase: () => {
-        return path.basename(process.cwd());
+        return path.basename(process.cwd())
     },
 
     directoryExists: filePath => {
         try {
-            return fs.statSync(filePath).isDirectory();
+            return fs.statSync(filePath).isDirectory()
         } catch (err) {
-            return false;
+            return false
         }
     },
     checkInit: () => {
-        if (!fs.exists("cliconf.json")) {
+        if (!fs.exists('cliconf.json')) {
             const question = {
-                name: "srcDirectory",
-                message: "Src directory?",
-                default: "src"
-            };
+                name: 'srcDirectory',
+                message: 'Src directory?',
+                default: 'src'
+            }
             inquirer.prompt(question).then(answers => {
-                console.log(answers);
-                fs.writeFile("cliconf.json", JSON.stringify(answers), err => {
+                console.log(answers)
+                fs.writeFile('cliconf.json', JSON.stringify(answers), err => {
                     if (err) {
-                        console.log("There has been an error saving your configuration data.");
-                        console.log(err.message);
-                        return;
+                        console.log('There has been an error saving your configuration data.')
+                        console.log(err.message)
+                        return
                     }
-                    console.log("Configuration saved successfully.");
-                    return Promise.resolve();
-                });
-            });
+                    console.log('Configuration saved successfully.')
+                    return Promise.resolve()
+                })
+            })
         } else {
-            return Promise.resolve();
+            return Promise.resolve()
         }
     }
-};
+}
